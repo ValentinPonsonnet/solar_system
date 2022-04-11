5952316ea04c9842f44617e443ce2fb81671554f
ValentinPonsonnet <Ponsonnetvalentin@gmail.com>
2022-04-11 12:13:59 +0200

    Ajout du tableau

---
+++ data/tab_continent.php
@@ -0,0 +1,54 @@
+<?php
+    $continent = 
+    [
+        "Mercure" => "Contient un seul et unqiue continent",
+        "Venus" => 
+        [
+            "Nord_Ouest" => "Les plateaux et de Lakshmi Planum",
+            "Sud_de_l'équateur"=>"Le plateau D'aphrodite Terra",
+            "Sud_Est" => "Alpha Regio",
+            "Extrême_Ouest" => "Bêta Regio"
+        ],
+        "Terre" => 
+        [
+          "1" => "L'amérique",
+          "2" => "L'Europe",
+          "3" => "L'Asie",
+          "4" => "L'afrique",
+          "5" => "L'Océanie",
+          "6" => "L'Antartique"
+        ],
+        "Mars" => 
+        [
+            "1" => "Catena",
+            "2" => "Cavus",
+            "3" => "Chaos",
+            "4" => "Chasma",
+            "5" => "Colles",
+            "6" => "Crater",
+            "7" => "Dorsum",
+            "8" => "Fluctus",
+            "9" => "Fossa",
+            "10" => "Labes",
+            "11" => "Labyrinthus",
+            "12" => "Lingula",
+            "13" => "Mensa",
+            "14" => "Mons",
+            "15" => "Patera",
+            "16" => "Palus",
+            "17" => "Planitia",
+            "18" => "Rupes",
+            "19" => "Scorpulus",
+            "20" => "Sulcus",
+            "21" => "Terra",
+            "22" => "Tholus",
+            "23" => "Unda",
+            "24" => "Vallis",
+            "25" => "Vastitas"
+        ],
+        "Jupiter" => "Continent de glace",
+        "Saturne" => "Composé de gaz",
+        "Uranus" => "Continent de glace",
+        "Neptune" => "Continent de glace"
+    ];
+?>
